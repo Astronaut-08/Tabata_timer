a6107da972a1a9a23d4509155cf3d0fe092e49d8
Volodymyr Yusyp <volodymyr.yusyp@gmail.com>
2025-08-14 20:53:52 +0300

    Тепер в базі тільки 6 колонок, колонки name більше немає, також добавлений метод витягування всіх id з бази даних

--- preset.py
+++ preset.py
@@ -1,5 +1,5 @@
 '''This is user preset for tabata timer power by SQLAlchemy'''
-from sqlalchemy import create_engine, MetaData, Column, Integer, Unicode
+from sqlalchemy import create_engine, MetaData, Column, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -12,42 +12,37 @@ class Preset(Base):
     '''This is the table of preset'''
     __tablename__='presets'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(Unicode, unique=True)
     work = Column(Integer)
     rest = Column(Integer)
     exercises = Column(Integer)
-    rouds = Column(Integer)
+    rounds = Column(Integer)
     rounds_reset = Column(Integer)
 
 Base.metadata.create_all(engine)
 
-def add_records(name='def', work=1, rest=1, exercises=1, rounds=1, rounds_reset=1):
+def add_records(work=1, rest=1, exercises=1, rounds=1, rounds_reset=1):
     '''Addicted records to the preset'''
     s = Session()
-    if name != s.query(Preset).filter_by(name = name).first().name:
-        new_record = Preset(name=name, work=work, rest=rest, exercises=exercises,\
-                            rouds=rounds, rounds_reset=rounds_reset)
-        s.add(new_record)
-        s.commit()
-    else:
-        return None
-
-def select_records_by_name(name):
-    '''Select records by name and return, to use try: select_records_by_name.row_name'''
-    s = Session()
-    res = s.query(Preset).filter_by(name = name).first()
-    return res
-
-def update_records_by_name(old_name, new_name):
-    '''Update records be the name'''
-    s = Session()
-    update_record = s.query(Preset).filter_by(name = old_name).first()
-    update_record.name = new_name
+    new_record = Preset(work=work, rest=rest, exercises=exercises,\
+                        rounds=rounds, rounds_reset=rounds_reset)
+    s.add(new_record)
     s.commit()
 
-def delete_records_by_name(name):
+def select_all_id():
+    '''Return all column of id'''
+    s = Session()
+    res = s.query(Preset).all()
+    return [row[0] for row in res]
+
+def select_records_by_id(user_id):
+    '''Select records by name and return, to use try: select_records_by_name.row_name'''
+    s = Session()
+    res = s.query(Preset).filter_by(id = user_id).first()
+    return res
+
+def delete_records_by_name(user_id):
     '''Delete records by the name'''
     s = Session()
-    delete_name = s.query(Preset).filter_by(name = name).first()
+    delete_name = s.query(Preset).filter_by(id = user_id).first()
     s.delete(delete_name)
     s.commit()
